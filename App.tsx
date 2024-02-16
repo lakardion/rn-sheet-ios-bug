c429dd000ee547031027039143da753e1ef2aea7
lakardion <lakardion@protonmail.com>
2024-02-16 13:53:40 -0300

    minimal-example-for bg interaction

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import ActionSheet, { ActionSheetRef } from "react-native-actions-sheet";
 
 export default function App() {
+  const actionSheetRef = useRef<ActionSheetRef>(null);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <View>
+          <Button
+            title="Open sheet"
+            onPress={() => {
+              actionSheetRef.current?.show();
+            }}
+          />
+          <View
+            style={{
+              paddingTop: 30,
+            }}
+          >
+            <Button
+              title="Close Sheet"
+              onPress={() => {
+                actionSheetRef.current?.hide();
+              }}
+            />
+          </View>
+        </View>
+      </View>
+      <ActionSheet
+        useBottomSafeAreaPadding
+        gestureEnabled={true}
+        defaultOverlayOpacity={0.3}
+        backgroundInteractionEnabled={true}
+        ref={actionSheetRef}
+      >
+        <View
+          style={{
+            height: 100,
+          }}
+        >
+          <Text>Hello from Sheet</Text>
+        </View>
+      </ActionSheet>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
